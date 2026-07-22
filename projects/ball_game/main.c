a33b25e7d1b79346422a7a6ae447bd25f25e917b
Masato Takatani <takatani.masato@gmail.com>
2026-07-21 21:40:32 -0600

    ballgame

--- projects/ball_game/main.c
+++ projects/ball_game/main.c
@@ -2,14 +2,32 @@
 
 int main()
 {
+  int ballX = 400;
+  int ballY = 400;
+  Color green = {20, 100, 133, 255};
+
   InitWindow(800, 800, "My first raylib Game");
+  SetTargetFPS(60);
 
   // game loop
   while (!WindowShouldClose()) {
     // 1. Event Handling
+    if (IsKeyDown(KEY_RIGHT)) {
+      ballX += 3;
+    } else if (IsKeyDown(KEY_LEFT)) {
+      ballX -= 3;
+    } else if (IsKeyDown(KEY_UP)) {
+      ballY -= 3;
+    } else if (IsKeyDown(KEY_DOWN)) {
+      ballY += 3;
+    }
+
     // 2. Updating Positions
     // 3. Dwawing
     BeginDrawing();
+    ClearBackground(green);
+    DrawCircle(ballX, ballY, 20, WHITE);
+
     EndDrawing();
   }
 
